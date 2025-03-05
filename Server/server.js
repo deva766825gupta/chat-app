39a579373e5bf14eb8e8b10c9579caf6d9c46091
DEVANSH GUPTA <121473435+deva766825gupta@users.noreply.github.com>
2025-03-05 19:24:51 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    "origin": ["https://deploy-mern-1whq.vercel.app"],
+    "origin": ["https://chat-app-o59v.vercel.app/"],
 
     methods: ["GET", "PUT"],
     credential:true
